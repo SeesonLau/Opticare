a18fbb47b9b447e834e3fb60f0f150848746be16
John Laurence Sison <seesonjohnlau@gmail.com>
2024-10-21 17:50:27 +0800

    Added component, pages, and styles in directories

---
+++ src/components/example.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/index.css'; 
+
+const Example = () => {
+    return (
+        <div className="component-example">
+            This is an example component.
+        </div>
+    );
+};
+
+export default Example;
