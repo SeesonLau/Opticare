8ac1dda67a4fa01fd3ea2378319ff0cc31186da4
Jamel Hadjirasul <hadjirasuljamel80@gmail.com>
2024-10-27 10:52:49 +0800

    sda

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import '../src/styles/index.css'
 import App from './page/landing-page';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>
 );
 
