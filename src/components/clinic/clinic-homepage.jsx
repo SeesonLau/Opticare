c48eb34b7721a086a09bcbedf70dc61d2ed39c8c
Jamel Hadjirasul <hadjirasuljamel80@gmail.com>
2024-12-15 00:16:27 +0800

    .

--- src/components/clinic/clinic-homepage.jsx
+++ src/components/clinic/clinic-homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PatientNavBar from '../patient/patient-navbar.jsx';
+
 import ClinicHome from './clinic-home.jsx';
 import ClinicProfile from './clinic-profile.jsx';
 import { Routes, Route } from 'react-router-dom';
