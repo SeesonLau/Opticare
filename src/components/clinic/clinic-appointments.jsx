3a9e342360f4644de5450f8088eefeeb19c31bc2
Lord Kent F. Dinampo <lordkentdinampo@gmail.com>
2024-12-15 00:30:42 +0800

    add files

---
+++ src/components/clinic/clinic-appointments.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/clinic/navbar/clinic-appointment.css';
+
+const ClinicAppointments = () => {
+  return (
+    <div className="appointments-container">
+      <h2 className="appointments-title">Appointments</h2>
+      <p className="appointments-placeholder">Clinic Appointments</p>
+    </div>
+  );
+};
+
+export default ClinicAppointments;
