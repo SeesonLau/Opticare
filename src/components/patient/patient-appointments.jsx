b67f1a5f1cb5327baad29dae2816bb09ab52bcd6
16-2042-807 John Augustine Lapinig <johnaugustine.lapinig@cit.edu>
2024-12-15 15:52:02 +0800

    TABS

--- src/components/patient/patient-appointments.jsx
+++ src/components/patient/patient-appointments.jsx
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/patient/navbar/patient-appointments.css'; 
 import '../../styles/appointments.css';
 
 
 const PatientAppointments = () => {
+  
+const [activeComponent, setActiveComponent] = useState('chats'); 
+
+
+  const handleComponentChange = (component) => {
+    setActiveComponent(component); // Updates the active component
+  };
+
+// Tab bar component
+const SettingsTabBar = ({ onComponentChange }) => {
+  const tabs = [
+  
+    { label: 'Chats', value: 'chats' },
+    { label: 'Book', value: 'book' },
+    { label: 'My Bookings', value: 'mybookings' },
+  ];
+
+  
+  return (
+    <div className="tab-bar">
+      {tabs.map((tab) => (
+        <button
+          key={tab.value}
+          className={`tab-button ${activeComponent === tab.value ? 'active' : ''}`}
+          onClick={() => onComponentChange(tab.value)}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+ 
+  
+  const Chats = () => (
+    <div>
+      Chats
+    </div>
+  );
+  
+  const Book= () => (
+    <div>
+      Book
+    </div>
+  );
+  
+  const MyBookings = () => (
+    <div>
+      My Bookings
+    </div>
+  );
+  
   return (
     <div className="appointments-container">
          <div className="content-wrapper">
          <div className="content">
       <h2 className="appointments-title">Appointments</h2>
+
+      <SettingsTabBar onComponentChange={handleComponentChange} />
+      
+      <div className="main-container">
+       
+        {activeComponent === 'chats' && <Chats />}
+        {activeComponent === 'book' && <Book />}
+        {activeComponent === 'mybookings' && <MyBookings />}
+      </div>
 
       </div>
       </div>
