15c8f94af31f372a9fe4b3db973fa3990fd1b18a
16-2042-807 John Augustine Lapinig <johnaugustine.lapinig@cit.edu>
2024-12-17 01:04:03 +0800

    modal balik

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -40,59 +40,91 @@ const Modal = ({ isOpen, onClose, title }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="modal1-overlay">
-            <div className="modal1-content">
+        <div className="modal-overlay">
+            <div className="modal-content">
                 <button
                     className="close-button"
                     onClick={onClose}
                     aria-label="Close Modal"
                 >
-                    <CloseIcon />
+                    <CloseIcon /> {/* Close icon */}
                 </button>
                 <h2>{title}</h2>
 
-                <div className="logo-and-user-type">
-                    <img className="opticare-logo" src={OpticareLogo} alt="Opticare Logo" />
-                    <div className="login-message">
-                        <span className="blue-text">Login to&nbsp;</span>
-                        <span className="green-text">Optimize&nbsp;</span>
-                        <span className="blue-dark-text">Your Eye Care Journey</span>
-                    </div>
+                {/* Logo and user type picker section */}
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '50px' }}>
+                    {/* Logo image aligned at the top center */}
+                    <img style={{ width: '300px', height: 'auto', marginRight: '50px', marginTop: '-100px' }} src={OpticareLogo} alt="Opticare Logo" />
 
-                    <div className="user-type-picker">
-                        <button
-                            onClick={() => handleToggleUserType('Patient')}
-                            className={`user-type-button ${
-                                userType === 'Patient' ? 'active' : ''
-                            }`}
+                    {/* Add the styled text below the logo and welcome message */}
+                    <div style={{ width: '100%', textAlign: 'right', marginRight: '300px', marginTop: '-90px', }}>
+                        <span style={{ color: '#3871C1', fontSize: '15px', fontFamily: 'Montserrat', fontWeight: 700, lineHeight: '40px', wordWrap: 'break-word' }}>
+                            Login to&nbsp;
+                        </span>
+                        <span style={{ color: '#6FB118', fontSize: '15px', fontFamily: 'Montserrat', fontWeight: 700, lineHeight: '40px', wordWrap: 'break-word' }}>
+                            Optimize&nbsp;
+                        </span>
+                        <span style={{ color: '#219DE9', fontSize: '15px', fontFamily: 'Montserrat', fontWeight: 700, lineHeight: '40px', wordWrap: 'break-word' }}>
+                            Your Eye Care Journey
+                        </span>
+                    </div>
+                    
+                    {/* User Type Picker Section */}
+                    <div style={{ display: 'flex', gap: '0px', marginTop: '30px', marginBottom: '50px' }}>
+                        <button 
+                            onClick={() => handleToggleUserType('Patient')} 
+                            style={{
+                                backgroundColor: userType === 'Patient' ? '#90DD2B' : '#FFFFFF',
+                                color: userType === 'Patient' ? 'white' : '#A1CBFF',
+                                padding: '10px 20px',
+                                border: '1px solid #4461F2',
+                                borderRadius: '7px',
+                                cursor: 'pointer',
+                            }}
                         >
                             Patient
                         </button>
-                        <button
-                            onClick={() => handleToggleUserType('Clinic')}
-                            className={`user-type-button ${
-                                userType === 'Clinic' ? 'active' : ''
-                            }`}
+                        <button 
+                            onClick={() => handleToggleUserType('Clinic')} 
+                            style={{
+                                backgroundColor: userType === 'Clinic' ? '#90DD2B' : '#FFFFFF',
+                                color: userType === 'Clinic' ? 'white' : '#A1CBFF',
+                                padding: '10px 20px',
+                                border: '1px solid #4461F2',
+                                borderRadius: '7px',
+                                cursor: 'pointer',
+                            }}
                         >
                             Clinic
                         </button>
                     </div>
                 </div>
 
-                <div className="email-input-section">
-                    <div className="input-wrapper">
+                {/* Email Input Section */}
+                <div style={{ paddingLeft: '275px', marginTop: '-70px' }}> {/* Reduced marginTop for higher positioning */}
+                    <div style={{ position: 'relative', display: 'flex', alignItems: 'right' }}>
                         <input
                             type="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder="Enter Email"
-                            className="input-field"
+                            style={{
+                                width: '250px',
+                                height: '40px',
+                                borderRadius: '5px',
+                                border: '1px solid #667085',
+                                paddingLeft: '10px',
+                                fontSize: '15px',
+                                fontFamily: 'Montserrat',
+                                fontWeight: 700,
+                                color: '#4F555A'
+                            }}
                         />
                         {email && (
                             <button
                                 onClick={handleClearEmail}
                                 style={{
-                                    position: 'absolute',
+                                    position: 'flex',
                                     left: '240px',
                                     top: '10px',
                                     border: 'none',
@@ -103,25 +135,36 @@ const Modal = ({ isOpen, onClose, title }) => {
                                 }}
                                 aria-label="Clear Email"
                             >
-                                &times;
+                                &times; {/* X icon for clearing text */}
                             </button>
                         )}
                     </div>
                 </div>
 
-                <div className="password-input-section">
-                    <div className="input-wrapper">
+                {/* Password Input Section */}
+                <div style={{ paddingLeft: '275px', marginTop: '20px' }}>
+                    <div style={{ position: 'relative', display: 'flex', alignItems: 'center' }}>
                         <input
                             type={showPassword ? 'text' : 'password'}
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             placeholder="Enter Password"
-                            className="input-field"
+                            style={{
+                                width: '250px',
+                                height: '40px',
+                                borderRadius: '5px',
+                                border: '1px solid #667085',
+                                paddingLeft: '10px',
+                                fontSize: '15px',
+                                fontFamily: 'Montserrat',
+                                fontWeight: 700,
+                                color: '#4F555A'
+                            }}
                         />
                         <button
                             onClick={togglePasswordVisibility}
                             style={{
-                                position: 'absolute',
+                                position: 'flex',
                                 left: '238px',
                                 top: '10px',
                                 border: 'none',
@@ -131,7 +174,7 @@ const Modal = ({ isOpen, onClose, title }) => {
                             }}
                             aria-label="Toggle Password Visibility"
                         >
-                            {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                            {showPassword ? <VisibilityOffIcon style={{ fontSize: '15px' }} /> : <VisibilityIcon style={{ fontSize: '15px' }} />}
                         </button>
                     </div>
                 </div>
@@ -142,7 +185,7 @@ const Modal = ({ isOpen, onClose, title }) => {
                         style={{
                             width: '250px',
                             height: '40px',
-                            position: 'absolute',
+                            position: 'flex',
                             background: 'linear-gradient(90deg, #4183CF 0%, #77B032 100%)',
                             boxShadow: '0px 11.851164817810059px 20.73953628540039px 3.950388193130493px rgba(68, 97, 242, 0.15)',
                             borderRadius: '20px',
@@ -154,7 +197,7 @@ const Modal = ({ isOpen, onClose, title }) => {
                     </div>
                     <div 
                         style={{
-                            position: 'absolute',
+                            position: 'flex',
                             left: '360px',
                             top: '-15px',
                             color: 'white',
@@ -174,16 +217,41 @@ const Modal = ({ isOpen, onClose, title }) => {
                     </div>
                 </div>
 
-                <div className="google-signin-section" onClick={() => alert('Google Sign-In Clicked')}>
-                    <img src={GoogleIcon} alt="Google Icon" className="google-icon" />
-                    <div className="google-signin-text">Continue with Google</div>
+                {/* "or" Separator Section */}
+                <div style={{ width: '280px', height: '100px', marginLeft: '260px', justifyContent: 'center', alignItems: 'center', gap: '24px', display: 'inline-flex', marginTop: '20px' }}>
+                    <div style={{ flex: '1 1 0', height: '1px', background: '#4183CF' }}></div>
+                    <div style={{ textAlign: 'center', color: '#4183CF', fontSize: '16px', fontFamily: 'Inter', fontWeight: 500, lineHeight: '24px', wordWrap: 'break-word' }}>
+                        or
+                    </div>
+                    <div style={{ flex: '1 1 0', height: '1px', background: '#4183CF' }}></div>
                 </div>
 
-                <div class="register-section">
-                <span class="register-info">if you don’t have an account, </span>
-                <span class="register-link"> Register Here!</span>
+                    {/* "Continue with Google" Section */}
+                    <div style={{ width: '250px', height: '100px', gap: '30px', marginLeft: '280px', marginTop: '-20px' }}>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center', // Align the icon and text vertically
+                        gap: '10px', // Adjust space between the icon and text
+                        cursor: 'pointer',
+                        padding: '10px 20px',
+                        borderRadius: '8px',
+                        border: '1px #4183CF solid', // Border around the section
+                        overflow: 'hidden', // Ensures the rounded corners are respected
+                        justifyContent: 'center',
+                    }} onClick={() => alert('Google Sign-In Clicked')}>
+                        <img src={GoogleIcon} alt="Google Icon" style={{ width: '20px', height: '20px' }} />
+                        <div style={{
+                            color: '#4183CF',
+                            fontSize: '14px',
+                            fontFamily: 'Inter',
+                            fontWeight: 500,
+                            lineHeight: '21px',
+                            wordWrap: 'break-word'
+                        }}>
+                            Continue with Google
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </div>
     );
