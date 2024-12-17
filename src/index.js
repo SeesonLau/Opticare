947eeaa7d4f9050658d73ee76040c62547b42e25
16-2042-807 John Augustine Lapinig <johnaugustine.lapinig@cit.edu>
2024-12-17 09:53:11 +0800

    login modal

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const HomePage = () => {
   };
   */
   const handleLoginClick = () => {
-    navigate('/patient-homepage');
+    setActiveModal('register'); // This will open the login modal
   };
 
   const handleCloseModal = () => {
